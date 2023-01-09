4db6b6cfd4e441a776add364abba9aef5031e341
Fernando <fermoraeslima10@gmail.com>
2023-01-09 10:51:10 -0300

    test 2

--- index.js
+++ index.js
@@ -4,33 +4,33 @@ const path = require('path');
 // const packageJson = require(path.resolve(__dirname, '..', '..', 'package.json'));
 const packageJson = require('./package-test.json');
 const semver = require('semver');
+const Table = require('cli-table');
+const request = require('request');
 
-const projectDependencies = Object.entries(packageJson.dependencies);
-const projectDevDependencies = Object.entries(packageJson.devDependencies);
-const args = process.argv && process.argv.slice(2) || [];
+const loadDependencies = async () => {
+  const url = 'https://raw.githubusercontent.com/ffernandomoraes/check-dependencies/master/dependencies.json';
 
-const frontCriticalDependencies = {};
-const frontMinVersionDependencies = {};
+  return await new Promise((resolve, reject) => {
+    request(url, { json: true }, (error, res, body) => {
+      if (error) reject(error);
+      if (!error && res.statusCode === 200) resolve(body);
+    });
+  });
+}
 
 const cleanVersion = (version) => semver.clean(version, { loose: true, includePrerelease: true }) || semver.valid(semver.coerce(version));
 
-const validateCriticalDependencies = (dependencies, criticalDependencies) => {
-  const errors = [];
-
-  dependencies.forEach(([name]) => {
+const validateCriticalDependencies = (dependencies, criticalDependencies, table) => {
+  dependencies.forEach(([name, version]) => {
     const criticalMessage = criticalDependencies[name];
 
     if (criticalMessage) {
-      errors.push(`[${name}]: ${criticalMessage}`);
+      table.push([name, version, criticalMessage]);
     }
   });
-
-  return errors;
 };
 
-const validateVersionDependencies = (dependencies, minVersionDependencies) => {
-  const warnings = [];
-
+const validateMinVersionDependencies = (dependencies, minVersionDependencies, table) => {
   dependencies.forEach(([name, version]) => {
     const minVersion = minVersionDependencies[name];
 
@@ -39,13 +39,33 @@ const validateVersionDependencies = (dependencies, minVersionDependencies) => {
       const currentVersion = cleanVersion(version);
 
       if (semver.gt(acceptableVersion, currentVersion)) {
-        warnings.push(`[${name}] versão atual: ${version}. Versão mínima aceitável: ${minVersion}`);
+        table.push([name, version, minVersion]);
       }
     }
   });
-
-  return warnings;
 };
 
-console.log(validateCriticalDependencies([...projectDependencies, ...projectDevDependencies], frontCriticalDependencies));
-console.log(validateVersionDependencies([...projectDependencies, ...projectDevDependencies], frontMinVersionDependencies));
+async function init() {
+  if (!packageJson) {
+    throw 'Não foi possível encontrar o arquivo package.json';
+    process.exit(1);
+  }
+
+  const criticalTable = new Table({ head: ['Dependência crítica', 'Versão atual', 'Observação'] });
+  const minVersionTable = new Table({ head: ['Dependência', 'Versão atual', 'Versão min. aceitável'], style: { head: ['blue'] }});
+
+  const configDependencies = await loadDependencies();
+  const projectDependencies = [...Object.entries(packageJson.dependencies), [...Object.entries(packageJson.devDependencies)]];
+
+  const currentValidationConfigDeps = configDependencies['front'];
+  const criticalDependencies = currentValidationConfigDeps['critical'];
+  const minVersionDependencies = currentValidationConfigDeps['libraries'];
+
+  validateCriticalDependencies(projectDependencies, criticalDependencies, criticalTable);
+  validateMinVersionDependencies(projectDependencies, minVersionDependencies, minVersionTable);
+
+  console.log(criticalTable.toString());
+  console.log(minVersionTable.toString());
+}
+
+init();
